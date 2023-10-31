2e679f77be999a65fb1f238a43cd285b2b349a9e
natal <svetnata74@mail.ru>
2023-10-31 12:35:36 +0100

    huhi

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,36 @@
 import pytest
-
+from appium.webdriver import webdriver
 
 from selene import browser
+from selenium.webdriver.chrome.options import Options
 
+from qa_guru.utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
-    # options = Options()
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": "100.0",
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #     }
-    # }
-    # options.capabilities.update(selenoid_capabilities)
-    # driver = webdriver.Remote(
-    #     command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-    #     options=options
-    # )
-    #
-    # browser.config.driver = driver
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
     browser.config.base_url = "https://demoqa.com"
     yield browser
 
-    # attach.add_screenshot(browser)
-    # attach.add_logs(browser)
-    # attach.add_html(browser)
-    # attach.add_video(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
