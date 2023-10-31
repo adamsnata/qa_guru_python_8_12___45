b0d9914f20de596c32733fc135b79bcebe135f1a
natal <svetnata74@mail.ru>
2023-10-31 12:21:47 +0100

    huhi

--- tests/test_demoqa_high.py
+++ tests/test_demoqa_high.py
@@ -1,6 +1,7 @@
 import allure
-
-from tests.registration_page import RegistrationPage
+from selene import browser, have
+#
+from users.users import User
 from users_data.user1_data import Vitalii_Sharov
 
 
@@ -14,3 +15,44 @@ def test_submit_student_registration_form_by_high_steps():
     with allure.step("Check form results"):
         registration_page.should_have_registrated_user(student)
 
+
+
+
+class RegistrationPage:
+    def open_form(self):
+        browser.open('/automation-practice-form')
+        return self
+
+    def submit_form(self, user: User):
+        browser.element('#firstName').type(user.first_name)
+        browser.element('#lastName').type(user.last_name)
+        browser.element('#userEmail').type(user.email)
+        browser.all('[name=gender]').element_by(have.value(user.gender)).element('..').click()
+        browser.element('#userNumber').type(user.number)
+        browser.element('#dateOfBirthInput').click()
+        browser.element('.react-datepicker__year-select').type(user.date_of_birth.year)
+        browser.element('.react-datepicker__month-select').type(user.date_of_birth.strftime('%B'))
+        browser.element(f'.react-datepicker__day--0{user.date_of_birth.day}').click()
+        browser.element('#subjectsInput').type(user.subject).press_enter()
+        browser.all('.custom-checkbox').element_by(have.exact_text(user.hobbies)).click()
+        browser.element('#uploadPicture').send_keys(resources.path(user.picture))
+        browser.element('#currentAddress').type(user.current_address)
+        browser.element('#react-select-3-input').type(user.state).press_enter()
+        browser.element('#react-select-4-input').type(user.city).press_enter().press_enter()
+
+    def should_have_registrated_user(self, user: User):
+        user.date_of_birth = f'{user.date_of_birth.day} {user.date_of_birth.strftime("%B")},{user.date_of_birth.year}'
+        browser.element('.table').all('td').even.should(
+            have.exact_texts(
+                f'{user.first_name} {user.last_name}',
+                user.email,
+                user.gender,
+                user.number,
+                user.date_of_birth,
+                user.subject,
+                user.hobbies,
+                user.picture,
+                user.current_address,
+                f'{user.state} {user.city}',
+            )
+        )
